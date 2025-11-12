644da8aa747674193a4c6f992807a09e7ebf9b83
Brett Koenig <brett9897@gmail.com>
2025-11-12 16:37:45 -0600

    Added cucumber

--- application-tests/build.gradle.kts
+++ application-tests/build.gradle.kts
@@ -2,19 +2,24 @@ plugins {
     kotlin("jvm")
 }
 
-group = "org.example"
-version = "unspecified"
+group = "warehouse-management.application-tests"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    testImplementation(project(":application"))
+
+    testImplementation(libs.bundles.cucumber)
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    systemProperty("cucumber.plugin", "pretty, html:build/reports/cucumber.html, json:build/reports/cucumber.json")
 }
 kotlin {
     jvmToolchain(21)
---
+++ application-tests/src/test/kotlin/steps/ReceiveInventorySteps.kt
@@ -0,0 +1,24 @@
+package steps
+
+import io.cucumber.java.en.Given
+import io.cucumber.java.en.When
+import io.cucumber.java.en.Then
+
+class ReceiveInventorySteps {
+    @Given("warehouse {string} exists")
+    fun warehouseExists(warehouseId: String) {
+        println("Setting up warehouse: $warehouseId")
+    }
+
+    @When("I receive {int} units of product {string}")
+    fun receiveProduct(units: Int, productSku: String) {
+        // TODO: Call application use case to receive inventory
+        println("Receiving $units units of $productSku")
+    }
+
+    @Then("the inventory should show {int} units of {string}")
+    fun verifyInventory(expectedUnits: Int, productSku: String) {
+        // TODO: Verify inventory level
+        println("Verifying $expectedUnits units of $productSku")
+    }
+}
