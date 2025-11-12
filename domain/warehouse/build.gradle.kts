6637552c5902ae45c19ca2c167d5367798138010
Brett Koenig <brett9897@gmail.com>
2025-11-12 17:46:25 -0600

    Warehouse implemented

--- domain/warehouse/build.gradle.kts
+++ domain/warehouse/build.gradle.kts
@@ -2,14 +2,16 @@ plugins {
     kotlin("jvm")
 }
 
-group = "org.example"
-version = "unspecified"
+group = "warehouse_management.domain.warehouse"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
     testImplementation(kotlin("test"))
 }
 
